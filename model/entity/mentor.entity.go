c1d23c22dd8418019b5a122f0c022fd42145b5fb
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-05-06 00:48:10 +0700

    fix: mentor entity data type

--- model/entity/mentor.entity.go
+++ model/entity/mentor.entity.go
@@ -1,12 +1,18 @@
 package entity
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Mentor struct {
-	FullName  string `json:"full_name"`
-	Company   string `json:"company"`
-	Specialty string `json:"specialty"`
-	Bio       string `json:"bio"`
-	Photo     string `json:"photo"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
-	DeletedAt string `json:"deleted_at"`
+	FullName  string         `json:"full_name"`
+	Company   string         `json:"company"`
+	Specialty string         `json:"specialty"`
+	Bio       string         `json:"bio"`
+	Photo     string         `json:"photo"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
