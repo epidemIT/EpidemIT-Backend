6714a98ad11aa61120d6c62a19cf2a8b4877ea6a
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-05-06 22:11:53 +0700

    feat: add project application endpoint logic

--- handler/project.handler.go
+++ handler/project.handler.go
@@ -2,10 +2,13 @@ package handler
 
 import (
 	"strconv"
+	"strings"
 
 	"github.com/epidemIT/epidemIT-Backend/database"
 	"github.com/epidemIT/epidemIT-Backend/model/dto"
 	"github.com/epidemIT/epidemIT-Backend/model/entity"
+	"github.com/epidemIT/epidemIT-Backend/utils"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -119,4 +122,69 @@ func ProjectHandlerCreate(c *fiber.Ctx) error {
 	}
 
 	return c.Status(200).JSON(responseDTO)
+}
+
+func ProjectApplyRegister(c *fiber.Ctx) error {
+	authHeader := c.Get("Authorization")
+
+	token := strings.TrimPrefix(authHeader, "Bearer ")
+	claims, err := utils.VerifyToken(token)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Error verifying token",
+			"error":   err.Error(),
+		})
+	}
+
+	body := new(dto.ProjectApplyRegisterRequestDTO)
+
+	if err := c.BodyParser(body); err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Error parsing new ProjectApply",
+			"error":   err.Error(),
+		})
+	}
+
+	validate := validator.New()
+	errValidate := validate.Struct(body)
+
+	if errValidate != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Error validating new ProjectApply",
+			"error":   errValidate.Error(),
+		})
+	}
+
+	var user entity.User
+	err = database.DB.Where("email = ?", claims["email"]).First(&user).Error
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Error getting user",
+			"error":   err.Error(),
+		})
+	}
+
+	var project entity.Project
+	err = database.DB.Where("id = ?", c.Params("id")).First(&project).Error
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Error getting project",
+			"error":   err.Error(),
+		})
+	}
+
+	project.Users = append(project.Users, user)
+
+	results := database.DB.Save(&project)
+
+	if results.Error != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Error applying to project",
+			"error":   results.Error,
+		})
+	}
+
+	return c.Status(201).JSON(fiber.Map{
+		"message": "Applied to project successfully",
+	})
 }
